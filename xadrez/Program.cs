26110652fb2f6c99a5f9a959671d70c05c3dcaa9
iurymagano <iurymagano17@live.com>
2024-05-28 23:02:55 -0300

    feat: Add class Posicao

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace xadrez
+﻿using tabuleiro;
+
+namespace xadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Posicao p;
 
+            p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
         }
     }
 }
